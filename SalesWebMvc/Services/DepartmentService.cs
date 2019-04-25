96380456b77334c59bf7d2baea26a482362a1418
Rafael Marinho <rafamola@gmail.com>
2019-04-25 16:26:48 -0300

    Projeto CRUD usando C# e ASP.NET

---
+++ SalesWebMvc/Services/DepartmentService.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SalesWebMvc.Models;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SalesWebMvc.Services
+{
+    public class DepartmentService
+    {
+        private readonly SalesWebMvcContext _context;
+
+        public DepartmentService(SalesWebMvcContext context)
+        {
+            _context = context;
+        }
+
+        //Metodo 'async' faz o acesso ao banco de forma assincrona, isto é,
+        //Não bloquea o banco até o final da execução do metodo.
+        public async Task<List<Department>> FindAllAsync()
+        {
+            return await _context.Department.OrderBy(x => x.Name).ToListAsync();
+        }
+    }
+}
